91005682b60890cf46fbd979d995ea51baa871e4
Rich Jones <rich@anomos.info>
2014-02-08 18:04:39 -0800

    total cost

--- billing/aws_billing/aws_billing.py
+++ billing/aws_billing/aws_billing.py
@@ -8,3 +8,11 @@ def get_cost_for_resource(resource_id):
     except Exception, e:
         print e
         return -1
+
+def get_total_cost():
+    try:
+        cost = BillingRecord.objects.all().aggregate(Sum('cost'))['cost__sum']
+        return cost
+    except Exception, e:
+        print e
+        return -1
